9ee3177ada58400a7e2994693ae8c187964f150f
Renkai Ye <realdream2016@outlook.com>
2018-01-10 11:24:05 +0800

    add some exceptional handling

--- mount.bash
+++ mount.bash
@@ -4,8 +4,36 @@ then
     echo "usage:$0 device"
     exit -1
 fi
-mkdir ./$1
-sudo dislocker-fuse -V /dev/$1 -u ./$1
-sudo mkdir /media/yrk16/$1
-sudo mount -t auto ./$1/dislocker-file /media/yrk16/$1
-exit
+device=$1
+# check if it is mounted 
+sudo test -e ./$device/dislocker-file
+if [ 0 -eq $? ]
+ then
+  echo "device:$device already mounted"
+  exit -1
+fi
+# check if the mount point of decrypted device exist
+sudo test -d ./$device
+if [ 0 -eq $? ]
+ then
+    sudo rm -r ./$device
+fi
+# do decrypt
+username=`users`
+mkdir ./$device
+sudo dislocker-fuse -V /dev/$device -u ./$device
+if [ $? -ne 0 ]
+then
+    echo "create decrypted device failed!"
+    echo "please check you password and the disk device!"
+    exit -1
+fi
+# check if the mount point of decrypted filesystem exist
+sudo test -d /media/$username/$device
+if [ 0 -eq $? ]
+ then
+    sudo rm -r /media/$username/$device
+fi
+sudo mkdir /media/$username/$device
+sudo chown $username:$username /media/$username/$device
+sudo mount -t auto -o uid=$username,gid=$username,dmask=022,fmask=133 ./$device/dislocker-file /media/$username/$device
--- unmount.bash
+++ unmount.bash
@@ -5,7 +5,15 @@ then
     exit -1
 fi
 sudo umount /media/yrk16/$1
-sudo rm -r /media/yrk16/$1
+sudo test -d /media/yrk16/$1
+if [ 0 -eq $? ]
+ then
+    sudo rm -r /media/yrk16/$1
+fi
 sudo umount ./$1
-sudo rm -r ./$1
+sudo test -d ./$1
+if [ 0 -eq $? ]
+ then
+    sudo rm -r ./$1
+fi
 exit
